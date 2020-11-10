732a7fbc2c14b8acd82efdf7e32a0e5e81b25325
MihaiD <mihaid_b@protonmail.com>
2020-11-10 13:06:20 +0200

    Revert "add document date picker"
    This reverts commit 7cdfa9e05b1615625527859877363454b8603ba9.
    
    add document date picker
    
    
    
    

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,8 +1,6 @@
 import React from "react";
 import DropboxChooser from "react-dropbox-chooser";
 import { Container, Row, Col, Table } from "react-bootstrap";
-import DocumentStore from '../stores/DocumentStore'
-import Document from './Document'
 
 class Main extends React.Component {
   constructor() {
@@ -13,22 +11,7 @@ class Main extends React.Component {
 
   render() {
     return (
-      <><nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href=""><i class="fas fa-plus"></i> Add Document</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="" title="Choose document from DropBox"><i class="fab fa-dropbox"></i> Dropbox</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <br /><br />
+      <>
         <Container>
           <Row>
             <Col>
@@ -43,9 +26,6 @@ class Main extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {
-                  this.state.documents.map(e=><Document key={e.id} item={e} />)
-                  }
                   <tr>
                     <td>1</td>
                     <td></td>
